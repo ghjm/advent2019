dead450c07bf0655aefb7e687bebc0e5a827a447
ghjm <graham@mhn.org>
2019-12-07 03:40:54 -0500

    Code cleanup

--- p7.py
+++ p7.py
@@ -9,10 +9,9 @@ with open("inputs/input7.txt", "r") as file:
 
 ic_prog = intcode([int(c) for c in content[0].split(',')])
 
-def worker_factory(thread_id, ic, in_q, out_q):
+def worker_factory(ic, in_q, out_q):
     def infunc():
-        val = in_q.get(block=True)
-        return val
+        return in_q.get(block=True)
     def outfunc(val):
         out_q.put(val)
     def worker():
@@ -20,39 +19,36 @@ def worker_factory(thread_id, ic, in_q, out_q):
     return worker
 
 def run_amplifiers(seq):
-    threads = list()
+
     queues = list()
+    for s in seq:
+        q = Queue()
+        q.put(s)
+        queues.append(q)
+
+    threads = list()
     for i in range(len(seq)):
-        queues.append(Queue())
-    for i in range(len(seq)):
-        ic = ic_prog.clone()
-        queues[i].put(seq[i])
-        t = threading.Thread(
-                target=worker_factory(i, ic, 
-                    queues[i], 
-                    queues[(i+1) % len(queues)]) 
-                )
-        threads.append(t)
+        threads.append(threading.Thread(target=worker_factory(
+            ic_prog.clone(), queues[i], queues[(i+1) % len(queues)])))
+
     for t in threads:
         t.start()
+
     queues[0].put(0)
+
     for t in threads:
         t.join()
+
     return queues[0].get()
 
-max_value = 0
-for seq in itertools.permutations(range(5)):
-    value = run_amplifiers(seq)
-    if value > max_value:
-        max_value = value
+def find_best(phases):
+    max_value = 0
+    for seq in itertools.permutations(phases):
+        value = run_amplifiers(seq)
+        if value > max_value:
+            max_value = value
+    return max_value
 
-print("Part 1:", max_value)
-
-max_value = 0
-for seq in itertools.permutations(range(5, 10)):
-    value = run_amplifiers(seq)
-    if value > max_value:
-        max_value = value
-
-print("Part 2:", max_value)
+print("Part 1:", find_best(range(5)))
+print("Part 2:", find_best(range(5, 10)))
 
