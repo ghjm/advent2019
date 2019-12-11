7d6d30eee5c8046a42f3b6b002da9dc66cfd3bba
ghjm <graham@mhn.org>
2019-12-11 02:26:05 -0500

    Rearranged code a bit

--- p11.py
+++ p11.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
-import threading
-from queue import Queue
 from intcode import intcode
-
-with open("inputs/input11.txt", "r") as file:
-    content = [line.rstrip() for line in file]
-ic_prog = intcode([int(c) for c in content[0].split(',')])
 
 directions = [(0, -1), (1, 0), (0, 1), (-1, 0)]  # U R D L
 
@@ -43,31 +37,39 @@ class Robot:
                 state = 0
         return outfunc
 
-robot = Robot()
-ic_prog.run(infunc=robot.get_infunc(), outfunc=robot.get_outfunc())
-print("Part 1:", len(robot.panel_colors))
+    def print_map(self):
+        min_x = min_y = max_x = max_y = 0
+        for pos in self.panel_colors:
+            if pos[0] < min_x:
+                min_x = pos[0]
+            if pos[0] > max_x:
+                max_x = pos[0]
+            if pos[1] < min_y:
+                min_y = pos[1]
+            if pos[1] > max_y:
+                max_y = pos[1]
+        
+        for y in range(min_y-1, max_y+2):
+            for x in range(min_x-1, max_x+2):
+                if (x,y) in self.panel_colors and self.panel_colors[(x,y)] == 1:
+                    print("#", end="")
+                else:
+                    print(" ", end="")
+            print()
 
-robot = Robot()
-robot.panel_colors[(0,0)] = 1
-ic_prog.run(infunc=robot.get_infunc(), outfunc=robot.get_outfunc())
 
-print("Part 2:")
-min_x = min_y = max_x = max_y = 0
-for pos in robot.panel_colors:
-    if pos[0] < min_x:
-        min_x = pos[0]
-    if pos[0] > max_x:
-        max_x = pos[0]
-    if pos[1] < min_y:
-        min_y = pos[1]
-    if pos[1] > max_y:
-        max_y = pos[1]
-
-for y in range(min_y, max_y+1):
-    for x in range(min_x, max_x+1):
-        if (x,y) in robot.panel_colors and robot.panel_colors[(x,y)] == 1:
-            print("#", end="")
-        else:
-            print(" ", end="")
-    print()
+if __name__ == '__main__':
+    with open("inputs/input11.txt", "r") as file:
+        content = [line.rstrip() for line in file]
+    ic_prog = intcode([int(c) for c in content[0].split(',')])
+    
+    robot = Robot()
+    ic_prog.run(infunc=robot.get_infunc(), outfunc=robot.get_outfunc())
+    print("Part 1:", len(robot.panel_colors))
+    
+    robot = Robot()
+    robot.panel_colors[(0,0)] = 1
+    ic_prog.run(infunc=robot.get_infunc(), outfunc=robot.get_outfunc())
+    print("Part 2:")
+    robot.print_map()
 
